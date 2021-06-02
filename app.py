917c912c7e641d946439f87f1c75d2f7e5c84ac7
GJSayers <gemma.sayers1@btinternet.com>
2021-06-02 11:51:40 +0000

    defin join functionality and create join object composed of form element submission

--- app.py
+++ app.py
@@ -24,8 +24,34 @@ def get_galleries():
     galleries = mongo.db.galleries.find()
     return render_template("galleries.html", galleries=galleries)
 
+
 @app.route("/join", methods=["GET", "POST"])
 def join():
+    if request.method == "POST":
+        """
+        checks if user is already a member in the database
+        """
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user: 
+            flash("Username already exists")
+            return redirect(url_for("join"))
+
+        join = {
+            "username": request.form.get("username").lower(),
+            "email": request.form.get("email"),
+            "password": generate_password_hash(request.form.get("password")),
+            "bio": request.form.get("bio"),
+            "profile-pic": request.form.get("profile-pic")
+        }  
+        mongo.db.users.insert_one(join)
+
+        """
+        start a session for the user with a session cookie
+        """
+        session["user"] = request.form.get("username").lower()
+        flash("Thanks for joining the Culturate community!")    
     return render_template("join.html")
 
 
@@ -33,4 +59,3 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-
