cdde943e2b9c7b497021a19ae2e7a97c369d58b0
GJSayers <gemma.sayers1@btinternet.com>
2021-06-02 15:43:48 +0000

    add logout logic

--- app.py
+++ app.py
@@ -100,7 +100,20 @@ def profile(username):
     """
     username = mongo.db.users.find_one(
         {"username":session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session ["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    """
+    remove session cookie on logout
+    """
+    session.pop("user")
+    return redirect(url_for("login"))
+
 
 
 if __name__ == "__main__":
