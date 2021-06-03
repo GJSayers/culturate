de64e9f180663b882bd0d321b1dc1a48ec74017b
GJSayers <gemma.sayers1@btinternet.com>
2021-06-03 16:16:09 +0000

    edit key to match schema and add recommendation submission functionality

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
-from flask_pymongo import PyMongo 
+from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
@@ -32,50 +32,51 @@ def join():
         checks if user is already a member in the database
         """
         existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
+            {"user_name": request.form.get("user_name").lower()})
 
-        if existing_user: 
+        if existing_user:
             flash("Username already exists")
             return redirect(url_for("join"))
 
         join = {
-            "username": request.form.get("username").lower(),
-            "email": request.form.get("email"),
-            "password": generate_password_hash(request.form.get("password")),
-            "bio": request.form.get("bio"),
-            "profile-pic": request.form.get("profile-pic")
-        }  
+            "user_name": request.form.get("user_name").lower(),
+            "user_email": request.form.get("user_email"),
+            "user_password": generate_password_hash(request.form.get("user_password")),
+            "user_bio": request.form.get("user_bio"),
+            "user_avatar": request.form.get("user_avatar")
+        }
         mongo.db.users.insert_one(join)
 
         """
         start a session for the user with a session cookie
         """
-        session["user"] = request.form.get("username").lower()
+        session["user"] = request.form.get("user_name").lower()
         flash("Thanks for joining the Culturate community!")
-        return redirect(url_for("profile", username=session["user"]))
+        return redirect(url_for("profile", user_name=session["user"]))
 
     return render_template("join.html")
 
-@app.route("/login", methods=["GET", "POST"])    
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         """
         checks if user is already a member in the database
         """
         existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
+            {"user_name": request.form.get("user_name").lower()})
 
         if existing_user:
             """
             checks password is correct
             """
             if check_password_hash(
-                    existing_user["password"], request.form.get("password")):
-                        session["user"] = request.form.get("username").lower()
-                        flash("Hey, {}".format(
-                            request.form.get("username")))
-                        return redirect(url_for(
-                            "profile", username=session["user"]))
+                    existing_user["user_password"], request.form.get("user_password")):
+                session["user"] = request.form.get("user_name").lower()
+                flash("Hey, {}".format(
+                    request.form.get("user_name")))
+                return redirect(url_for(
+                    "profile", user_name=session["user"]))
 
             else:
                 """
@@ -93,18 +94,19 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/profile/<username>", methods=["GET", "POST"])
-def profile(username):
+@app.route("/profile/<user_name>", methods=["GET", "POST"])
+def profile(user_name):
     """
     get session user's username from the database
     """
-    username = mongo.db.users.find_one(
-        {"username":session["user"]})["username"]
+    user_name = mongo.db.users.find_one(
+        {"user_name": session["user"]})["user_name"]
 
-    if session ["user"]:
-        return render_template("profile.html", username=username)
+    if session["user"]:
+        return render_template("profile.html", user_name=user_name)
 
     return redirect(url_for("login"))
+
 
 @app.route("/logout")
 def logout():
@@ -115,11 +117,27 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/add_recommendation")
+@app.route("/add_recommendation", methods=["GET", "POST"])
 def add_recommendation():
+    if request.method == "POST":
+        recommendation = {
+            "category_name": request.form.get("category_name"),
+            "gallery_facilities": request.form.getlist("gallery_facilities"),
+            "gallery_name": request.form.get("gallery_name"),
+            "gallery_cost": request.form.get("gallery_cost"),
+            "gallery_rating": request.form.get("gallery_rating"),
+            "gallery_city": request.form.get("gallery_city"),
+            "gallery_comments": request.form.get("gallery_comments"),
+            "gallery_image": request.form.get("gallery_image"),
+            "created_by": session["user"]
+        }
+
+        mongo.db.galleries.insert_one(recommendation)
+        flash("Recommendation Added - Thank you!")
+        return redirect(url_for("get_galleries"))
+
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_recommendation.html", categories=categories)
-
 
 
 if __name__ == "__main__":
