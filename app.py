7d7c8c9ff6ae1b6483c751a7d7a1b0e99551dc66
GJSayers <gemma.sayers1@btinternet.com>
2021-06-08 15:53:15 +0000

    typo fix

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ def edit_category(category_id):
 @app.route("/delete_category/<category_id>")
 def delete_category(category_id):
     mongo.db.categories.remove({"_id": ObjectId(category_id)})
-    flash("Category Successfully deteted")
+    flash("Category Successfully deleted")
     return redirect(url_for("get_categories"))
 
 
