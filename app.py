c1c32a53e66451f1c279bca222332810513ae885
GJSayers <gemma.sayers1@btinternet.com>
2021-06-08 13:09:50 +0000

    add edit categories route

--- app.py
+++ app.py
@@ -194,8 +194,20 @@ def add_category():
         mongo.db.categories.insert_one(category)
         flash("New Category Added, Thank you!")
         return redirect(url_for("get_categories"))
-        
+
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    if request.method == "POST":
+        submit = {
+            "category_name": request.form.get("category_name")
+        }
+        mongo.db.categories.update({"_id": ObjectId(category_id)}, submit)
+        flash("Category Successfully Updated - Thank you!")
+    category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
+    return render_template("edit_category.html", category=category)
 
 
 if __name__ == "__main__":
