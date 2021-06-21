9c296cff3a5752ce0e20748a9c71ce675d511c00
GJSayers <gemma.sayers1@btinternet.com>
2021-06-21 12:34:50 +0000

    fix display for fab buttons on listings page

--- static/js/script.js
+++ static/js/script.js
@@ -4,21 +4,12 @@ $(document).ready(function () {
     $('.parallax').parallax();
     $('.collapsible').collapsible();
     $('.carousel').carousel();
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-      direction: 'left',
-      hoverEnabled: false
+    $('.fixed-action-btn').floatingActionButton({
+        direction: 'left',
+        hoverEnabled: false,
+        isOpen: false
     });
-  });
-
-instance.open();
-
-instance.close();
- 
-    
+});
 
 
  validateMaterializeSelect();
